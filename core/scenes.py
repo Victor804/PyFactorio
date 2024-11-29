3e05ac1fee79a5756b559730f6db80ba79ba3de3
victor804 <victor.vaucel@gmail.com>
2024-11-30 00:19:37 +0100

    feat: generate map scene added

--- core/scenes.py
+++ core/scenes.py
@@ -1,12 +1,13 @@
 import pygame.locals
 from core.map import Map, MapRenderer
 from core.config import Config
+import multiprocessing
 
 class SceneManager:
     def __init__(self, game):
         self.game = game
-        self.current_scene = MainScene(game)
-
+        self.current_scene = GenerateMapScene(game)
+        
     def handle_events(self):
         self.current_scene.handle_events()
 
@@ -17,11 +18,47 @@ class SceneManager:
         self.current_scene.render(screen)
 
 
-
-class MainScene:
+class GenerateMapScene:
     def __init__(self, game):
         self.game = game
-        self.map_renderer = MapRenderer(Map(), Config.TILES_SIZE)
+        self.map = None
+        self.is_generating = True
+
+        self.queue = multiprocessing.Queue()
+        self.process = multiprocessing.Process(target=self.generate_map, args=(self.queue,))
+        self.process.start()
+
+    def generate_map(self, queue):
+        generated_map = Map()
+        queue.put(generated_map)
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.game.running = False
+
+    def update(self):
+        if self.is_generating and not self.queue.empty():
+            self.map = self.queue.get()
+            self.is_generating = False
+            self.game.scene_manager.current_scene = MainScene(self.game, self.map)
+
+    def render(self, screen):
+        screen.fill((0, 0, 0))
+        if self.is_generating:
+            font = pygame.font.Font(None, 36)
+            text = font.render("Generating Map...", True, (255, 255, 255))
+            screen.blit(text, (50, 50))
+        else:
+            font = pygame.font.Font(None, 36)
+            text = font.render("Map Generated! Transitioning...", True, (0, 255, 0))
+            screen.blit(text, (50, 50))
+
+
+class MainScene:
+    def __init__(self, game, generated_map=None):
+        self.game = game
+        self.map_renderer = MapRenderer(generated_map if generated_map else Map(), Config.TILES_SIZE)
 
     def handle_events(self):
         for event in pygame.event.get():
